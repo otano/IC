ff5a2beb3f487cd416dfaf0d056d8ec6f9e7354d
Solweg <lngouguenheim@hotmail.fr>
2025-12-04 10:49:36 +0100

    mise à jours BDDbibliothque

--- src/main/java/bibliotheque/BDDbilbio.java
+++ src/main/java/bibliotheque/BDDbilbio.java
@@ -5,14 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class BDDbilbio {
-    private static final String URL = "";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+    // L'hôte est le nom du service Docker : 'mariadb'
+    private static final String URL = "jdbc:mysql://mariadb:3306/bibliotheque"; 
+    
+    // Identifiants correspondant à votre docker-compose.yml
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "rootpassword"; 
 
     public static Connection getConnection() throws SQLException {
         try {
+            // Nécessite la dépendance MySQL Connector/J !
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
+            System.err.println("Driver JDBC non trouvé.");
             e.printStackTrace();
         }
         return DriverManager.getConnection(URL, USER, PASSWORD);
